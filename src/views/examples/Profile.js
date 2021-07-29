d39a12a72946b3e7a65d44d7c752c43128f4d25b
mahmoudabubaker9 <m7moud.abubaker.92@gmail.com>
2021-07-29 21:08:11 +0300

    finish editing

--- src/views/examples/Profile.js
+++ src/views/examples/Profile.js
@@ -23,7 +23,7 @@ import { Button, Card, Container, Row, Col } from "reactstrap";
 // core components
 import DemoNavbar from "components/Navbars/DemoNavbar.js";
 import SimpleFooter from "components/Footers/SimpleFooter.js";
-import profile from "assets/img/theme/team-4-800x800.jpg";
+import profile from "/home/mahmoudabubaker/code301/Portfolio-template/src/assets/img/theme/mahmoud.jpg";
 
 class Profile extends React.Component {
   componentDidMount() {
@@ -107,48 +107,43 @@ class Profile extends React.Component {
                     </Col>
                     <Col className="order-lg-1" lg="4">
                       <div className="card-profile-stats d-flex justify-content-center">
+
                         <div>
-                          <span className="heading">22</span>
-                          <span className="description">Friends</span>
+                          <span className="heading">Languages</span>
+                          <span className="description">HTML,CSS,JavaScript,React</span>
                         </div>
                         <div>
-                          <span className="heading">10</span>
-                          <span className="description">Photos</span>
+                          <span className="heading">Projects</span>
+                          <span className="description">5</span>
                         </div>
-                        <div>
-                          <span className="heading">89</span>
-                          <span className="description">Comments</span>
-                        </div>
+
                       </div>
                     </Col>
                   </Row>
                   <div className="text-center mt-5">
                     <h3>
-                      Jessica Jones{" "}
-                      <span className="font-weight-light">, 27</span>
+                      Mahmoud Abubaker{" "}
                     </h3>
                     <div className="h6 font-weight-300">
                       <i className="ni location_pin mr-2" />
-                      Bucharest, Romania
+                      Amman - Jordan
                     </div>
                     <div className="h6 mt-4">
                       <i className="ni business_briefcase-24 mr-2" />
-                      Solution Manager - Creative Tim Officer
+                      Software Development Student
                     </div>
                     <div>
                       <i className="ni education_hat mr-2" />
-                      University of Computer Science
+                      Luminus Technical University College
                     </div>
                   </div>
                   <div className="mt-5 py-5 border-top text-center">
                     <Row className="justify-content-center">
                       <Col lg="9">
                         <p>
-                          An artist of considerable range, Ryan — the name taken
-                          by Melbourne-raised, Brooklyn-based Nick Murphy —
-                          writes, performs and records all of his own music,
-                          giving it a warm, intimate feel with a solid groove
-                          structure. An artist of considerable range.
+                        I started to learn design and programming  through programs and websites available on the Internet to enhance my knowledge
+                      , then I begun to develop this hobby , i tried to learn intensively to acquire it as a skill and a work environment,
+                      I start looking for academies and platforms to learn programming ، to gain knowledge and skills.
                         </p>
                         <a href="#pablo" onClick={e => e.preventDefault()}>
                           Show more
